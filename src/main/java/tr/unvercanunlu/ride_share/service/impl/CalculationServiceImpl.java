ad74cf008c271486b9a0ea2abf53d8657d21074c
unver <unvercanunlu@gmail.com>
2025-07-19 20:52:27 +0200

    logging and error handling added.

--- src/main/java/tr/unvercanunlu/ride_share/service/impl/CalculationServiceImpl.java
+++ src/main/java/tr/unvercanunlu/ride_share/service/impl/CalculationServiceImpl.java
@@ -3,6 +3,7 @@ package tr.unvercanunlu.ride_share.service.impl;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import tr.unvercanunlu.ride_share.config.AppConfig;
+import tr.unvercanunlu.ride_share.helper.LogHelper;
 import tr.unvercanunlu.ride_share.service.CalculationService;
 
 public class CalculationServiceImpl implements CalculationService {
@@ -10,12 +11,29 @@ public class CalculationServiceImpl implements CalculationService {
   @Override
   public BigDecimal calculatePrice(double distance) {
     if (distance < 0) {
+      LogHelper.error(this.getClass(),
+          String.format("Distance cannot be negative: distance=%f", distance));
+
       throw new IllegalArgumentException("Distance cannot be negative: %f".formatted(distance));
     }
 
-    return BigDecimal.valueOf(
-        AppConfig.BASE_FARE + (distance * AppConfig.KM_RATE)
-    ).setScale(2, RoundingMode.HALF_UP);
+    try {
+      BigDecimal price = BigDecimal.valueOf(
+          AppConfig.BASE_FARE + (distance * AppConfig.FARE_KM_RATE)
+      ).setScale(2, RoundingMode.HALF_UP);
+
+      LogHelper.info(this.getClass(),
+          String.format("Price calculated. distance=%f price=%f", distance, price));
+
+      return price;
+
+    } catch (Exception e) {
+      LogHelper.error(this.getClass(),
+          String.format("Failed to calculate price. distance=%.2f, error=%s", distance, e.getMessage()));
+
+      throw e;
+    }
+
   }
 
 }
