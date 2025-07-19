a6f03626d493f341a4bfabf7d32b1f203698f57f
unver <unvercanunlu@gmail.com>
2025-07-20 00:57:20 +0200

    logging and error handling added.

--- src/main/java/tr/unvercanunlu/ride_share/service/impl/GeoServiceImpl.java
+++ src/main/java/tr/unvercanunlu/ride_share/service/impl/GeoServiceImpl.java
@@ -3,18 +3,51 @@ package tr.unvercanunlu.ride_share.service.impl;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import tr.unvercanunlu.ride_share.entity.Location;
+import tr.unvercanunlu.ride_share.helper.LogHelper;
 import tr.unvercanunlu.ride_share.service.GeoService;
 
 public class GeoServiceImpl implements GeoService {
 
   @Override
   public double calculateDistance(Location start, Location end) {
-    return getRandom().nextInt(3, 50);
+    LogHelper.info(this.getClass(),
+        String.format("Calculating distance: start=%s, end=%s", start, end));
+
+    try {
+      double distance = getRandom().nextInt(3, 50);
+
+      LogHelper.info(this.getClass(),
+          String.format("Distance calculated: start=%s, end=%s, distance=%.2f", start, end, distance));
+
+      return distance;
+
+    } catch (Exception e) {
+      LogHelper.error(this.getClass(),
+          String.format("Failed to calculate distance: start=%s, end=%s, error=%s", start, end, e.getMessage()));
+
+      throw e;
+    }
   }
 
   @Override
   public int estimateDuration(Location start, Location end) {
-    return getRandom().nextInt(3, 50);
+    LogHelper.info(this.getClass(),
+        String.format("Estimating duration: start=%s, end=%s", start, end));
+
+    try {
+      int duration = getRandom().nextInt(3, 50);
+
+      LogHelper.info(this.getClass(),
+          String.format("Duration estimated: start=%s, end=%s, duration=%d", start, end, duration));
+
+      return duration;
+
+    } catch (Exception e) {
+      LogHelper.error(this.getClass(),
+          String.format("Failed to estimate duration: start=%s, end=%s, error=%s", start, end, e.getMessage()));
+
+      throw e;
+    }
   }
 
   private static Random getRandom() {
